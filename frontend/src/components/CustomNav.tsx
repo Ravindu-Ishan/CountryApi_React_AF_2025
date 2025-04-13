67b287b671e48116a32ccdff13ef11dc14e0df1e
Ravindu Ishan <IT22358752@my.sliit.lk>
2025-04-13 18:52:54 +0530

    refactor

--- frontend/src/components/CustomNav.tsx
+++ frontend/src/components/CustomNav.tsx
@@ -1,13 +1,31 @@
 import { Navbar } from "flowbite-react";
+import { useState, useEffect } from "react";
+import { ToggleSwitch } from "flowbite-react";
 
 export function CustomNav() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (isDarkMode) {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
-    <Navbar fluid rounded className="max-w-full">
-      <div className="w-full flex justify-end">
-        <a href="#" className="text-sm text-blue-600 hover:underline mt-2 mr-2">
+    <Navbar
+      fluid
+      rounded
+      className="max-w-full bg-transparent dark:bg-transparent"
+    >
+      <div className="flex w-full justify-end">
+        <a href="#" className="text-md mt-2 mr-2 text-black hover:underline">
           Signup
         </a>
       </div>
+      <ToggleSwitch checked={isDarkMode} label="Toggle me" onChange={setIsDarkMode} />
     </Navbar>
   );
 }
